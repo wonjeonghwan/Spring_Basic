8568b04ca39ca4a700dd84450af7cf8e4802969b
wonjeonghwan <wonjeonghwan@naver.com>
2025-11-11 22:08:53 +0900

    Spring board project launched

---
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -0,0 +1,59 @@
+package com.study.board.controller;
+
+import org.springframework.ui.Model;
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+
+public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
+
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+    @GetMapping("/board/write") //localhost:8080/board/write
+    public String boardwritingform(){
+
+        return "boardwrite";
+    }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board) {
+
+        boardService.write(board);
+
+        return "";
+    }
+
+    @GetMapping("/board/list")
+    public String boardlist(Model model) {
+
+        model.addAttribute("list", boardService.boardList());
+
+        return "boardlist";
+    }
+
+    @GetMapping("/board/view") // localhost:8080/board/view?id=1
+    public String boardView(Model model, Integer id) {
+
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardview";
+    }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id) {
+
+        boardService.boardDelete(id);
+
+        return "redirect:/board/list";
+    }
+}
